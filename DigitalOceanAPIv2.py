d55d745dc2d4ff84323f27d62aa95db3126ccf25
hax <hax@kali-hax.localdomain>
2020-12-13 14:00:52 -0500

    Add DO droplet creation functionality

---
+++ DigitalOceanAPIv2.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+"""
+Based off of https://github.com/sowdowdow/digital-ocean-api/
+"""
+
+from requests import post, get, delete
+
+class DigitalOceanAPIv2(object):
+    def __init__(self, bearer: str):
+        self._url = 'https://api.digitalocean.com/v2/'
+        self._headers = {
+            'content-type': 'application/json',
+            'Authorization': f"Bearer {bearer}",
+        }
+
+    def list_all_actions(self):
+        r = get(self._url + 'droplets', headers=self._headers)
+        return r.json()
+
+    def create_droplet(self, region: str, size: str, image: str, name: str, tag: str, sshkeyid: str):
+        data = {
+            "name": name,
+            "region": region ,
+            "size": size,
+            "image": image,
+            "tag" : tag,
+            "SSHKeys" : [sshkeyid]
+        }
+        r = post(self._url + 'droplets/', headers=self._headers, json=data)
+        return r.json()
+
+    def list_droplets(self, **kwargs):
+        r = get(self._url + 'droplets', headers=self._headers, params=kwargs)
+        return r.json()
+
+    def user_informations(self):
+        r = get(self._url + 'account', headers=self._headers)
+        return r.json()
+
+    def delete_droplet(self, id: int):
+        """
+        Delete a droplet
+        :param id: the id of the droplet to delete
+        :return: dict representing the status of the request
+        """
+        r = delete(self._url + f'droplets/{id}', headers=self._headers)
+        print(r.status_code)
+        if r.status_code == 204:
+            return {'status': 'deleted',
+                    'message': f'droplet with id [{id}] was deleted successfully'}
+        else:
+            return r.text
+
+    def list_regions(self, **kwargs):
+        r = get(self._url + 'regions', headers=self._headers, params=kwargs)
+        return r.json()
+
+    def list_images(self, result_per_page: int = 20, **kwargs):
+        r = get(self._url + 'images', headers=self._headers, params={'per_page': result_per_page, **kwargs})
+
+        return r.json()
+
+    def list_distribution_images(self, result_per_page: int = 20):
+        return self.list_images(result_per_page=result_per_page, type='distribution')
+
+    def rate_limit(self):
+        """
+        Return an array representing the number of requests that can be made
+        through the API is currently limited to 5,000 per hour per OAuth token
+
+        :return: A dict containing the limit per hour, remaining and date of reset for the oldest request
+        """
+        r = get(self._url, headers=self._headers)
+        return {
+            "Limit": r.headers['Ratelimit-Limit'],
+            "Remaining": r.headers['Ratelimit-Remaining'],
+            "Reset": r.headers['Ratelimit-Reset'],
+        }
--- dovpn.py
+++ dovpn.py
@@ -8,7 +8,23 @@ import socket
 import subprocess
 import yaml
 
-import DigitalOceanAPIv2
+from DigitalOceanAPIv2 import DigitalOceanAPIv2
+
+
+import logging
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
 
 DO_API_DOMAIN = """api.digitalocean.com"""
 
@@ -27,6 +43,8 @@ def setup_iptables_rules(config, custom_rules):
     iptables("-F")
     iptables("-A INPUT -i lo -j ACCEPT")
     iptables("-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT")
+    iptables("-A OUTPUT -o lo -j ACCEPT")
+    iptables("-A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT")
     iptables("-A OUTPUT -o {} -p udp -d {} --dport 53 -j ACCEPT".format(
         config["net"]["interface"],
         config["net"]["dns"]
@@ -51,12 +69,19 @@ def setup_iptables_rules(config, custom_rules):
     iptables("-nvL", output=True)
 
 def setup_iptables_for_do_api(config):
-    do_ip = socket.gethostbyname(DO_API_DOMAIN)
+    # DO API has several IPs, try to get them all!
+    do_ips = []
+    for i in range(10):
+        do_ip = socket.gethostbyname(DO_API_DOMAIN)
+        if do_ip not in do_ips:
+            do_ips.append(do_ip)
+
     custom_rules = []
-    custom_rules.append("-A OUTPUT -o {} -p tcp -d {} --dport 443 -j ACCEPT".format(
-        config["net"]["interface"],
-        do_ip
-    ))
+    for do_ip in do_ips:
+        custom_rules.append("-A OUTPUT -o {} -p tcp -d {} --dport 443 -j ACCEPT".format(
+            config["net"]["interface"],
+            do_ip
+        ))
     setup_iptables_rules(config, custom_rules)
 
 def setup_iptables_for_vpn(config, droplet_ip):
@@ -76,6 +101,17 @@ def setup_iptables_for_vpn(config, droplet_ip):
     ))
     custom_rules.append("-A OUTPUT -o tun0 -j ACCEPT")
     setup_iptables_rules(config, custom_rules)
+
+def get_vpn_droplet(config, do_api):
+    r= do_api.create_droplet(
+        name = config["do"]["droplet"]["prefix"] + str(random.randint(0, 100000)),
+        image = config["do"]["droplet"]["image"],
+        region = config["do"]["droplet"]["region"],
+        size = config["do"]["droplet"]["size"],
+        tag = config["do"]["droplet"]["tag"],
+        sshkeyid = config["do"]["sshkeyid"]
+    )
+    print(r)
 
 def main():
     parser = argparse.ArgumentParser(description='Manage a DigitalOcean VPN.')
@@ -97,6 +133,9 @@ def main():
 
     setup_iptables_for_do_api(config_yaml)
 
+    do_api = DigitalOceanAPIv2(config_yaml["do"]["apikey"])
+    get_vpn_droplet(config_yaml, do_api)
+    
     setup_iptables_for_vpn(config_yaml, "1.2.3.4")
     
 
